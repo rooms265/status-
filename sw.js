36d76d2918725cfe95d4e8565f17b410331bf291
rooms265 <pramodp362@gmail.com>
2025-11-20 20:49:58 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,2 @@
+self.addEventListener('install', function(e){ e.waitUntil(caches.open('v1').then(function(cache){ return cache.addAll(['./','./index.html']); })); });
+self.addEventListener('fetch', function(e){ e.respondWith(caches.match(e.request).then(function(r){ return r || fetch(e.request); })); });
